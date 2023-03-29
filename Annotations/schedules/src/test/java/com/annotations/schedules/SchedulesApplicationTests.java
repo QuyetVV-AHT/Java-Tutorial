0e03814478199be069f32bbe04387481e72af3b3
QuyetVVNAL <quyetvv@nal.vn>
2023-03-29 15:01:48 +0900

    Init project

---
+++ Annotations/schedules/src/test/java/com/annotations/schedules/SchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annotations.schedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
